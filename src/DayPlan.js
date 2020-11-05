1803d401805e94b40d7d11a5bdef1fc735a6c227
O.K <mail4nu@i.ua>
2020-11-05 10:51:24 +0200

    app with array reportSetting [id, title, isDone] at the core

--- src/DayPlan.js
+++ src/DayPlan.js
@@ -3,201 +3,384 @@ import './App.css'
 
 
 class StatButton extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {name: 'все що потрібно знати про свої звички'};
-        this.handleClick = this.handleClick.bind(this);
-    }
+  constructor(props){
+    super(props);
+    this.state = {name: 'all you need to know about your habits'};
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    handleClick() {
-        this.props.updateData(this.state.name);
-    }
+  handleClick() {
+    this.props.updateMessage(this.state.name);
 
-    render() {
-        return (
-            <button
-                type="button"
-                className="btn btn-secondary"
-                onClick= {() => this.handleClick()}
-            >
-                Stat
-            </button>
-        );
-    }
+    // тут треба вивести весь Report
+
+	  let updateStatMessage ="";
+	  for (let i=0; i< this.props.Report.length;i++) {
+
+		  updateStatMessage +=  (
+		  	this.props.Report[i].date
+			  +  ": "
+			  +
+		    this.props.Report[i].reportSetting.map(
+		    	(h) => h.title + (h.isDone?' + ':' - ')
+		    )
+		  )
+		  + "\n";
+		//  ????????????????чому не працює перенос рядка????????????????????????
+	  }
+     this.props.updateReportMessage(updateStatMessage);
+  }
+
+  render() {
+    return (
+      <button
+        type="button"
+        className="btn btn-secondary"
+        onClick= {() => this.handleClick()}
+      >
+        Stat
+      </button>
+    );
+  }
 }
-
 
 class SettingsButton extends React.Component {
-    state = {
-        name: 'налаштування звичок'
-    };
+  state = {
+    name: 'habit settings'
+  };
 
-    handleClick() {
-        this.props.updateData(this.state.name);
-    }
+  handleClick() {
+    this.props.updateData(this.state.name);
+    this.props.handleActiveModeChange('configuration','#4e8dc6','#6c757d');
+  }
 
-    render() {
-        return (
-            <button
-                type="button"
-                className="btn btn-secondary"
-                onClick= {() => this.handleClick()}
-            >
-                Settings
-            </button>
-        );
-    }
+  render() {
+    return (
+      <button
+        type="button"
+        style = {{background:this.props.buttonColor}}
+        className="btn btn-secondary"
+        onClick= {() => this.handleClick()}
+      >
+        Configure
+      </button>
+    );
+  }
 }
 
+class ExecuteModeButton extends React.Component {
+	handleClick() {
+		this.props.handleActiveModeChange('execution', '#4e8dc6','#6c757d');
+	}
+
+	render() {
+		return (
+			<button
+				type="button"
+				style = {{background:this.props.buttonColor}}
+				className="btn btn-secondary"
+				onClick= {() => this.handleClick()}
+			>
+				Execute
+			</button>
+		);
+	}
+}
 
 class SaveButton extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {name: 'Звичка - з сейвбаттона'}
+  constructor(props){
+    super(props);
+    this.state = {
+      name: `Звички за цей день збережено`,
     }
+  }
 
-    handleClick() {
-        this.props.updateData(this.state.name);
-    }
+  handleClick() {
+    this.props.updateMessage(this.state.name);
+    this.props.updateReport(
+      this.props.Report,
+      this.props.reportDate,
+      this.props.reportSetting
+    );
+
+    let updateReportMessage =
+      this.props.Report[this.props.Report.length-1].date
+      +  ": " +
+      this.props.Report[this.props.Report.length-1].reportSetting.map(
+        (h) =>
+          h.title + (h.isDone?' + ':' - ')
+      );
+    this.props.updateReportMessage(updateReportMessage);
+  }
 
     render() {
-        return (
-            <button
-                type="button"
-                className="btn btn-secondary"
-                onClick= {() => this.handleClick()}
-            >
-                Save the day
-            </button>
-        );
+	    return (
+		    <button
+			    type="button"
+			    className="btn btn-outline-secondary"
+			    style={{width:400}}
+			    // className="btn btn-secondary"
+			    onClick= {() => this.handleClick()}
+		    >
+			    Save
+		    </button>
+	    );
     }
 }
 
 
-function CheckHabit(props) {
-    return (
-    <div className="input-group mb-3" style={{width:400}}>
-        <div className="input-group-prepend">
-            <div className="input-group-text">
-                <input type="checkbox" aria-label="Checkbox for following text input" defaultChecked={props.isDone}  />
-            </div>
-        </div>
-        <input type="text" className="form-control" aria-label="Text input with checkbox" value={props.title}/>
-    </div>)
+class ConfigureHabit extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleHabitListChange = this.handleHabitListChange.bind(this);
+		this.delHabitListItem = this.delHabitListItem.bind(this);
+	}
+
+	handleHabitListChange(e) {
+		this.props.handleHabitListChange(e.target.id, e.target.value);
+	}
+
+	delHabitListItem() {
+		this.props.delHabitListItem(this.props.habitsList, this.props.indexItem)
+	}
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.delHabitListItem}>
+					<div className="input-group">
+						<input
+							type="text"
+							className="form-control"
+							aria-describedby="button-addon4"
+							defaultValue={this.props.habit.title}
+							onChange={this.handleHabitListChange}
+							id={this.props.habit.id}
+						/>
+						<div className="input-group-append" id="button-addon4">
+							<input
+								className="btn btn-outline-secondary"
+								type="submit"
+								value = "Delete"
+							>
+							</input>
+							= {this.props.habit.id} + {this.props.habit.title} + {this.props.indexItem}
+						</div>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
 
+class CheckHabit extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+  }
+
+  handleCheckboxChange(e) {
+    this.props.handleCheckboxChange(e.target.id);
+  }
+
+  render() {
+    return (
+      <div className="input-group mb-3" style={{width:400}}>
+        {/*<div className="input-group-prepend">*/}
+          <div className="input-group-text">
+            <input
+              type="checkbox"
+              aria-label="Checkbox for following text input"
+              onChange={this.handleCheckboxChange}
+              defaultChecked={this.props.isDone}
+              id={this.props.id}
+            />
+          </div>
+        {/*</div>*/}
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Text input with checkbox"
+          value={this.props.title}
+          readOnly={true}
+        />
+        = {this.props.id} + {String(this.props.isDone)} + {this.props.title}
+      </div>
+    )
+  }
+}
 
 function DayAlert(props) {
-    const {doneMessageState} = props;
-    return (
-        <div className="alert alert-light" role="alert" align="center" style={{width:400}} >
-            {doneMessageState}
-        </div>
-    )
+  const {Message} = props;
+  return (
+    <div className="alert alert-light"
+         role="alert"
+         align="center"
+         style={{width:400}} >
+      {Message}
+    </div>
+  )
 }
 
 class DayHead  extends React.Component{
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleChange(e) {
-        this.props.onDateChange(e.target.value);
-    }
+  handleChange(e) {
+    this.props.updateReportDate(e.target.value);
+  }
 
-    render() {
-        const reportDate = this.props.reportDate;
-        console.log(reportDate);
-        return(
-               <div className="DayPlan">
-                    <div className="alert alert-dark" role="alert"  align="center" style={{width:400}}>
-                        Трекер корисних звичок
-                    </div>
+  render() {
+    const reportDate = this.props.reportDate;
+    return(
+      <div className="DayPlan">
+        <div className="alert alert-dark" role="alert"  align="center" style={{width:400}}>
+          Habit tracker
+        </div>
 
-                    <div className="input-group input-group-sm mb-3" style={{width:400}}> </div>
-
-                    <div className="input-group input-group-sm mb-3" style={{width:400}}>
-                        <div className="input-group-prepend">
-                            <span className="input-group-text" id="inputGroup-sizing-sm">сьогодні</span>
-                        </div>
-                        <input type="date" className="form-control"
-                               aria-label="Sizing example input"
-                               aria-describedby="inputGroup-sizing-sm"
-                               defaultValue={reportDate}
-                               // defaultValue={this.now()}
-
-                               onChange={this.handleChange}
-                               value ={reportDate}
-                        />
-                    </div>
-                    <DayAlert doneMessageState = {this.props.doneMessageState}/>
-                </div>
-        )
-    }
+        {/*<div className="input-group input-group-sm mb-3" style={{width:400}}> </div>*/}
+        <div className="input-group input-group-sm mb-3" style={{width:400}}>
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="inputGroup-sizing-sm">date</span>
+          </div>
+          <input type="date" className="form-control"
+                 aria-label="Sizing example input"
+                 aria-describedby="inputGroup-sizing-sm"
+                 defaultValue={reportDate}
+                 onChange={this.handleChange}
+          />
+        </div>
+        <DayAlert Message = {this.props.Message}/>
+      </div>
+    )
+  }
 }
 
 class DayPlan extends React.Component{
-    constructor(props) {
-        super(props);
-        this.now = this.now.bind(this);
-        this.handleDateChange = this.handleDateChange.bind(this);
-        this.state = {
-            doneMessageState:  'Звичка - початкове повідомлення пр звичку',
-            reportDate: this.now(),
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      Message:  'start message about useful habits',
+      reportMessage: '',
     }
+  }
 
-    now() {
-        let curDate = new Date();
-        let year = curDate.getFullYear();
-        let month = String(curDate.getMonth()+1).padStart(2, '0');
-        let day = String(curDate.getDate()).padStart(2,'0');
-        return year+'-'+month+'-'+day
+  updateMessage = (value) => {
+    this.setState({ Message: value });
+  };
+
+  updateReportMessage =(value) => {
+    this.setState({reportMessage:value});
+  };
+
+	addHabitListItem = () => {
+		this.props.addHabitListItem(this.props.habitsList,"")
+	};
+
+  setMode = () => {
+    if (this.props.activeMode === 'execution') {
+      return (
+        <div >
+          {this.props.reportSetting.map((h, index) =>
+            <CheckHabit
+              title = {h.title}
+              isDone = {h.isDone}
+              handleCheckboxChange = {this.props.handleCheckboxChange}
+              id = {h.id}
+              key={index}/>)
+          }
+	        <SaveButton
+		        reportSetting = {this.props.reportSetting}
+		        updateMessage={this.updateMessage}
+		        updateReportMessage={this.updateReportMessage}
+		        reportDate = {this.props.reportDate}
+		        updateReport = {this.props.updateReport}
+		        Report = {this.props.Report}
+	        />
+	        <div>
+		        <DayAlert Message={this.state.reportMessage}/>
+	        </div>
+        </div>
+      )
+
+    } else if (this.props.activeMode === 'configuration') {
+      return (
+        <div style={{width:400}}>
+	        {this.props.habitsList.map((h,index) =>
+	        <ConfigureHabit
+		        habit = {h}
+		        indexItem={index}
+		        habitsList = {this.props.habitsList}
+		        delHabitListItem = {this.props.delHabitListItem}
+		        handleHabitListChange = {this.props.handleHabitListChange}
+	        />)}
+	        <button
+		        type="button"
+		        className="btn btn-outline-secondary"
+		        style={{width:400}}
+		        onClick={this.addHabitListItem}
+	        >
+		        Add new habit
+	        </button>
+        </div>
+      )
+    } else {
+      return  (
+        <div>
+          Something is wrong with Active Mode
+        </div>
+      )
     }
+  };
 
-    handleDateChange(reportDate) {
-        this.setState({reportDate})
-    }
+  render() {
+    return (
+      <div className="DayPlan">
+        <div>
+          <DayHead
+            Message = {this.state.Message}
+            updateReportDate = {this.props.updateReportDate}
+            reportDate = {this.props.reportDate}
+          />
 
-    updateData = (value) => {
-        this.setState({ doneMessageState: value });
-    };
+          <div>
+            <div className="btn-group" role="group" aria-label="Basic example" style={{width:400}}>
+              <ExecuteModeButton
+                handleActiveModeChange = {this.props.handleActiveModeChange}
+                buttonColor = {this.props.saveButtonColor}
+              />
 
-    render() {
-        const reportDate = this.state.reportDate;
-        return (
-            <div className="DayPlan">
-                <div>
-                    <DayHead
-                        doneMessageState = {this.state.doneMessageState}
-                        onDateChange = {this.handleDateChange}
-                        reportDate = {reportDate}
-                    />
-
-                    {this.props.habits.map((h, index) =>
-                        <CheckHabit title = {h.title} isDone = {h.isDone} key={index}/>)}
-
-                    <div>
-                        <div className="btn-group" role="group" aria-label="Basic example" style={{width:400}}>
-                            <SaveButton updateData={this.updateData} />
-                            <StatButton updateData={this.updateData} />
-                            <SettingsButton updateData={this.updateData} />
-                        </div>
-
-                        <div>
-                            <DayAlert doneMessageState={this.state.doneMessageState}/>
-                        </div>
-
-                        <div>
-                            {reportDate}
-                        </div>
-                    </div>
-                </div>
+              <SettingsButton
+                updateData={this.updateMessage}
+                handleActiveModeChange = {this.props.handleActiveModeChange}
+                buttonColor = {this.props.settingButtonColor}
+              />
             </div>
-        );
-    }
 
+	          <br/><br/>
+
+	          <div>
+		          {/*---------2-------------*/}
+		          {/*чому не хоче передаватися як параметр функції*/}
+		          <this.setMode
+			          activeMode = {this.props.activeMode}
+		          />
+	          </div>
+
+            <div className="btn-group"  style={{width:400}}>
+              <StatButton
+                Report = {this.props.Report}
+                updateMessage={this.updateMessage}
+                updateReportMessage={this.updateReportMessage}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default DayPlan;
--- src/HabitTracker.js
+++ src/HabitTracker.js
@@ -3,26 +3,146 @@ import './App.css'
 import DayPlan from "./DayPlan";
 
 class HabitTracker extends React.Component{
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {doneMessage : 'Звичка - з хебіттрекера'     };
-    // }
+  constructor(props) {
+    super(props);
 
-    // updateData = (value) => {
-    //     this.setState({ name: value });
-    //     // console.log(this.name)
-    // };
-    render() {
+    //********************---------1-------------***************
+    // без цього можна обходитися????
+    // this.now = this.now.bind(this);
+    // this.ID = this.ID.bind(this);
 
-        return (
-            <div className="HabitTracker">
-                <div>
-                      {<DayPlan habits = {this.props.habits} />}
-                </div>
-            </div>
-        );
+    this.state = {
+      habitsList: [
+        {id:0, title:'Study English'},
+        {id:1, title:'Study React'},
+        {id:2, title:'Drink water'},
+        {id:3, title:'No smoking'},
+        {id:4, title:'Smile'}
+        ],
+      reportSetting: [
+        {id: 0, title: 'Study English', isDone: false},
+        {id: 1, title: 'Study React', isDone: true},
+        {id: 2, title: 'Drink water', isDone: false},
+        {id: 3, title: 'No smoking', isDone: false},
+        {id: 4, title: 'Smile', isDone: false}
+      ],
+      reportDate: this.now(),
+      Report: [],
+      activeMode: 'execution',
+      // activeMode: 'configuration',
+      saveButtonColor:'#4e8dc6',
+      settingButtonColor:'#6c757d',
     }
+    };
 
+  now() {
+    let curDate = new Date();
+    let year = curDate.getFullYear();
+    let month = String(curDate.getMonth()+1).padStart(2, '0');
+    let day = String(curDate.getDate()).padStart(2,'0');
+    return year+'-'+month+'-'+day
+  };
+
+  ID () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  };
+
+  //note: active button #4e8dc6' blue,    inactive button #6c757d gray
+
+  handleActiveModeChange = (value,active, inactive) => {
+    this.setState({activeMode:value});
+    if (value === 'execution') {
+      this.setState({saveButtonColor:active, settingButtonColor:inactive},
+    )} else if (value === 'configuration') {
+      this.setState({saveButtonColor: inactive, settingButtonColor:active},
+      )
+    }
+  };
+
+  handleDateChange = (reportDate) => {
+    this.setState({reportDate})
+  };
+
+  handleCheckboxChange = (targetId) => {
+    this.setState({
+      ...this.state,
+      reportSetting: this.state.reportSetting.map(list => {
+        if (String(list.id) === String(targetId)) {
+          return {
+            ...list,
+            isDone: !list.isDone,
+          }
+        } else {
+          return list;
+        }
+      })
+    })
+  };
+
+  handleHabitListChange = (targetId, targetTitle) => {
+    this.setState({
+      ...this.state,
+      habitsList: this.state.habitsList.map(list => {
+        if (String(list.id) === String(targetId)) {
+          return {
+            ...list,
+            title: targetTitle,
+          }
+        } else {
+          return list;
+        }
+      })
+     })
+  };
+
+  addHabitListItem = (habitsList, item) => {
+    habitsList.push({
+      id:this.ID(),
+      title:item,
+    });
+    this.setState({habitsList});
+};
+
+  delHabitListItem = (habitsList, item) => {
+    habitsList.splice(item,1);
+    this.setState({habitsList});
+  };
+
+  updateReport = (Report, date, reportSetting) => {
+    let smthId = Report.find(smth => smth.date === date);
+    if (!smthId) {
+      Report.push({
+        id: this.ID(),
+        date: date,
+        reportSetting:reportSetting, });
+      this.setState({Report});
+    } else {alert('Запис за ' +date+' уже існує')}
+  };
+
+  render() {
+    return (
+      <div className="HabitTracker">
+        <div>
+          <DayPlan
+            habitsList = {this.state.habitsList}
+            reportSetting = {this.state.reportSetting}
+            reportDate = {this.state.reportDate}
+            updateReportDate = {this.handleDateChange}
+            Report = {this.state.Report}
+            updateReport = {this.updateReport}
+            handleCheckboxChange = {this.handleCheckboxChange}
+            handleHabitListChange = {this.handleHabitListChange}
+            addHabitListItem = {this.addHabitListItem}
+            delHabitListItem = {this.delHabitListItem}
+            activeMode = {this.state.activeMode}
+            handleActiveModeChange = {this.handleActiveModeChange}
+            saveButtonColor = {this.state.saveButtonColor}
+            settingButtonColor = {this.state.settingButtonColor}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default HabitTracker;
